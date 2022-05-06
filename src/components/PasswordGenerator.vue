5fc899b994b0c8877dcbcf34468080843d4e6c2d
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-05-06 15:29:36 +0200

    Update PasswordGenerator

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="password-generator">
-        Content
+
+        <div class="row">
+            Amount <input type="number" v-model="amountOfCharacters">
+        </div>
+
+        <div class="row">
+            Symbols <input type="checkbox" v-model="symbols">
+        </div>
+
+        <div class="row">
+            Your generated password: <input type="text" v-model="generatedPassword" disabled>
+        </div>
+
+        <div class="row">
+            <button @click="generatePassword()">Generate</button>
+        </div>
+
     </div>
 </template>
 
@@ -9,11 +25,25 @@ export default {
     name: 'PasswordGenerator',
     data() {
         return {
-            
+            generatedPassword: '',
+            amountOfCharacters: 6,
+            symbols: true
         }
     },
     methods: {
+        generatePassword() {
+            let password = '';
+            let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let charactersLength = characters.length;
+            for (let i = 0; i < this.amountOfCharacters; i++) {
+                password += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
 
+            this.generatedPassword = password;
+        }
+    },
+    beforeMount() {
+        this.generatePassword();
     }
 }
 </script>
@@ -21,6 +51,17 @@ export default {
 <style lang="scss" scoped>
 .password-generator {
     font-size: 2rem;
-    background-color: red;
+    background-color: rgb(135, 107, 107);
+
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-auto-rows: 1fr;
+
+    .row {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
 }
 </style>
