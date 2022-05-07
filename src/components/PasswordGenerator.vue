c6032608bac74d49d9bd61ac93e7e3aa236d1a74
macieeek <iwaniukmaciej.kontakt@gmail.com>
2022-05-07 12:38:55 +0200

    Change way of assigning characters to generate password from

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -10,11 +10,23 @@
         </div>
 
         <div class="row">
-            Your generated password: <input type="text" v-model="generatedPassword" disabled>
+            Digits <input type="checkbox" v-model="digits">
         </div>
 
         <div class="row">
-            <button @click="generatePassword()">Generate</button>
+            Small letters <input type="checkbox" v-model="smallLetters">
+        </div>
+
+        <div class="row">
+            Big letters <input type="checkbox" v-model="bigLetters">
+        </div>
+
+        <div class="row">
+            Generated password: <input type="text" class="generatedPassword" v-model="generatedPassword" disabled>
+        </div>
+
+        <div class="row">
+            <button class="generateButton" @click="regeneratePassword(); getSymbols();">Generate</button>
         </div>
 
     </div>
@@ -25,25 +37,78 @@ export default {
     name: 'PasswordGenerator',
     data() {
         return {
-            generatedPassword: '',
             amountOfCharacters: 6,
-            symbols: true
+            symbols: true,
+            digits: true,
+            smallLetters: true,
+            bigLetters: true
         }
     },
     methods: {
-        generatePassword() {
-            let password = '';
-            let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-            let charactersLength = characters.length;
-            for (let i = 0; i < this.amountOfCharacters; i++) {
-                password += characters.charAt(Math.floor(Math.random() * charactersLength));
-            }
-
-            this.generatedPassword = password;
+        /**
+         * Return symbols as string
+         */
+        getSymbols() {
+            // specials chars in ascii - (from -> to)
+            // (33 -> 47), (58 -> 64), (91 -> 96), (123 -> 126)
+            let symbols = '';
+            for (let code = 33; code <= 47; code++) symbols += `${String.fromCharCode(code)}`;
+            for (let code = 58; code <= 64; code++) symbols += `${String.fromCharCode(code)}`;
+            for (let code = 91; code <= 96; code++) symbols += `${String.fromCharCode(code)}`;
+            for (let code = 123; code <= 126; code++) symbols += `${String.fromCharCode(code)}`;
+            return symbols;
+        },
+        /**
+         * Returns digits as string
+         */
+        getDigits() {
+            // digits in ascii - (48 -> 57)
+            let digits = '';
+            for (let code = 48; code <= 57; code++) digits += `${String.fromCharCode(code)}`;
+            return digits;
+        },
+        /**
+         * Returns small letters as string
+         */
+        getSmallLetters() {
+            // small letters in ascii - (97 -> 122)
+            let smallLetters = '';
+            for (let code = 97; code <= 122; code++) smallLetters += `${String.fromCharCode(code)}`;
+            return smallLetters;
+        },
+        /**
+         * Returns big letters as string
+         */
+        getBigLetters() {
+            // big letters in ascii - (65 -> 90)
+            let bigLetters = '';
+            for (let code = 65; code <= 90; code++) bigLetters += `${String.fromCharCode(code)}`;
+            return bigLetters;
+        },
+        /**
+         * Focuses regenerating password via changing amount of characters
+         */
+        regeneratePassword() {
+            this.amountOfCharacters -= 1;
+            this.amountOfCharacters += 1;
         }
     },
-    beforeMount() {
-        this.generatePassword();
+    computed: {
+        generatedPassword() {
+            let password = '',
+                characters = '';
+
+            if (this.symbols) characters += `${this.getSymbols()}`;
+            if (this.digits) characters += `${this.getDigits()}`;
+            if (this.smallLetters) characters = `${this.getSmallLetters()}`;
+            if (this.bigLetters) characters = `${this.getBigLetters()}`;
+
+            for (let i = 0; i < this.amountOfCharacters; i++) {
+                password += characters.charAt(Math.floor(Math.random() * characters.length));
+            }
+
+            return password;
+        }
     }
 }
 </script>
@@ -61,6 +126,28 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
+    }
+
+    .generatedPassword {
+        width: 13rem;
+        height: 3rem;
+        font-size: 1.2rem;
+        color: black;
+    }
+
+    .generateButton {
+        width: 14rem;
+        height: 4rem;
+        font-size: 1.6rem;
+        font-weight: 700;
+        border-radius: 15px;
+        border: solid black 2px;
+        background-color: white;
+        color: black;
+    }
+    .generateButton:hover {
+        cursor: pointer;
+        background-color: rgb(240, 233, 206);
     }
 
 }
